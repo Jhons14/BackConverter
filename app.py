d8a6def402d07afd363b0123853e1438a8368634
Jhons14 <tivenorjuela7@gmail.com>
2024-10-02 15:36:10 -0500

    Se agrega prettier en las respuestas

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
+import json
 from flask import Flask, request, jsonify
 from src.converter import json_to_xml, xml_to_json
-import json
 from dicttoxml import dicttoxml
-import xmltodict
+from flask_cors import CORS  # Importar CORS
+from xml.dom.minidom import parseString
 
 app = Flask(__name__)
+CORS(app)  # Habilitar CORS para todas las rutas
+
+
+def prettify_xml(xml_string):
+    dom = parseString(xml_string)
+    return dom.toprettyxml()
+
+def prettify_json(json_data):
+    return json.dumps(json_data, indent=4, ensure_ascii=False)
 
 @app.route('/convert/xml-to-json', methods=['POST'])
 def convert_xml_to_json():
     xml_data = request.data.decode('utf-8')  # Obtener XML del body de la petición
     try:
-        json_result = xml_to_json(xml_data)  # Convertir XML a JSON
-        return jsonify(json_result), 200  # Devolver el JSON convertido
+        json_result = xml_to_json(xml_data)  # Convertir XML a JSON'
+        pretty_json = prettify_json(json_result)
+        return jsonify(pretty_json), 200  # Devolver el JSON convertido
     except Exception as e:
         return jsonify({'error': str(e)}), 400  # Manejo de errores
 
@@ -20,7 +31,8 @@ def convert_json_to_xml():
     json_data = request.json  # Obtener JSON del body de la petición
     try:
         xml_result = json_to_xml(json.dumps(json_data))  # Convertir JSON a XML
-        return xml_result, 200, {'Content-Type': 'application/xml'}  # Devolver el XML convertido
+        xml_pretty = prettify_xml(xml_result) 
+        return xml_pretty, 200, {'Content-Type': 'application/xml'}  # Devolver el XML convertido
     except Exception as e:
         return jsonify({'error': str(e)}), 400  # Manejo de errores
 
--- src/converter.py
+++ src/converter.py
@@ -1,11 +1,13 @@
+
 import json
-from dicttoxml import dicttoxml
 import xmltodict
+from dicttoxml import dicttoxml
+
 
 def json_to_xml(json_data):
     data_dict = json.loads(json_data)
     xml_data = dicttoxml(data_dict, custom_root='output', attr_type=False)
-    return xml_data.decode()
+    return xml_data
 
 def xml_to_json(xml_data):
     data_dict = xmltodict.parse(xml_data)
